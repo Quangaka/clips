6fbacc3e89e2333d991abf3edc9d5413829e3dc2
Trương Thành Quang <thanhquangq998@gmail.com>
2022-10-21 16:15:09 +0700

    Copy links to the Clipboard

--- src/app/modules/video/components/manage/manage.component.ts
+++ src/app/modules/video/components/manage/manage.component.ts
@@ -86,4 +86,18 @@ export class ManageComponent implements OnInit {
     })
   }
 
+  async copyToClipBoard($event: MouseEvent, docID: string | undefined) {
+    $event.preventDefault();
+
+    if(!docID) {
+      return;
+    }
+
+    const url = `${location.origin}/clip/${docID}`;
+
+    await navigator.clipboard.writeText(url);
+
+    alert('Link Copied!');
+  }
+
 }
